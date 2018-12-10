da0f0da61ab8910e1ab4032c3ad59f63c9058b12
Jordi Orts <jorts64@gmail.com>
2018-12-10 19:05:15 +0100

    Afegit exemple RTC
    	fitxer nou:       examples/RTC_test/RTC_test.ino

---
+++ examples/RTC_test/RTC_test.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include "RTClib.h"
+
+RTC_DS1307 RTC;
+
+void setup () {
+    Serial.begin(9600);
+    Wire.begin();
+    RTC.begin();
+  // Check to see if the RTC is keeping time.  If it is, load the time from your computer.
+  if (! RTC.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // This will reflect the time that your sketch was compiled
+    RTC.adjust(DateTime(__DATE__, __TIME__));
+  }
+}
+void loop () {
+    DateTime now = RTC.now(); 
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print('/');
+    Serial.print(now.year(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();    
+    delay(1000);
+}
+
