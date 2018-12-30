c1717e1f653ee1e102639ed587eefc602fe8194c
Jordi Orts <jorts64@gmail.com>
2018-12-30 21:27:48 +0100

    Ara el GY-521 utilitza el plotter serie
    	modificat:        examples/GY-521/GY-521.ino

--- examples/GY-521/GY-521.ino
+++ examples/GY-521/GY-521.ino
@@ -1,7 +1,3 @@
- // MPU-6050 Short Example Sketch
-// By Arduino User JohnChi
-// August 17, 2014
-// Public Domain
 #include<Wire.h>
 const int MPU_addr=0x68;  // I2C address of the MPU-6050
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
@@ -25,13 +21,13 @@ void loop(){
   GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  Serial.print("AcX = "); Serial.print(AcX);
-  Serial.print(" | AcY = "); Serial.print(AcY);
-  Serial.print(" | AcZ = "); Serial.print(AcZ);
-  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
-  Serial.print(" | GyX = "); Serial.print(GyX);
-  Serial.print(" | GyY = "); Serial.print(GyY);
-  Serial.print(" | GyZ = "); Serial.println(GyZ);
+  Serial.print(AcX); Serial.print(",");
+  Serial.print(AcY); Serial.print(",");
+  Serial.print(AcZ); Serial.print(",");
+  Serial.print(GyX); Serial.print(","); 
+  Serial.print(GyY); Serial.print(","); 
+  Serial.print(GyZ); Serial.print(","); 
+  Serial.println(Tmp/340.00+36.53);  //equation for temperature in degrees C from datasheet
   delay(333);
 }
 
