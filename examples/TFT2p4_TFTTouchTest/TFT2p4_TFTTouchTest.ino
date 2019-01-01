212eb39861c0b2de4b0c880619a1489e14b6cc4b
Jordi Orts <jorts64@gmail.com>
2019-01-01 21:17:56 +0100

    Afegits exemples TFT 2.4 touch
    	fitxer nou:       examples/TFT2p4_TFTTouchTest/TFT2p4_TFTTouchTest.ino
    	fitxer nou:       examples/TFT2p4_touchpaint/TFT2p4_touchpaint.ino

---
+++ examples/TFT2p4_TFTTouchTest/TFT2p4_TFTTouchTest.ino
@@ -0,0 +1,68 @@
+/***************************************************
+1. !!!Make sure you using lastest ESP8266 core for Arduino, otherwise it may not work properly.
+  https://github.com/esp8266/Arduino
+  
+  (The pin D0(GPIO16) will be not work when you use older version ESP8266 core for Arduino, 
+  because the older version ESP8266 core for Arduino's digitalPinToBitMask(), portOutputRegister(), 
+  portInputRegister() and portModeRegister() fuction have some bugs which Adafruit_ILI9341 Library will use.
+  This bug was fixed after commit:  https://github.com/esp8266/Arduino/commit/799193888a553de8876052019842538396f92194 )
+    
+2. Setup latest Adafruit_GFX, Adafruit_ILI9341 and XPT2046_Touchscreen Library first:
+    https://github.com/adafruit/Adafruit-GFX-Library
+    https://github.com/adafruit/Adafruit_ILI9341
+    https://github.com/PaulStoffregen/XPT2046_Touchscreen
+****************************************************/
+
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#include <XPT2046_Touchscreen.h>
+
+#define TFT_CS D0  //for D1 mini or TFT I2C Connector Shield (V1.1.0 or later)
+#define TFT_DC D8  //for D1 mini or TFT I2C Connector Shield (V1.1.0 or later)
+#define TFT_RST -1 //for D1 mini or TFT I2C Connector Shield (V1.1.0 or later)
+#define TS_CS D3   //for D1 mini or TFT I2C Connector Shield (V1.1.0 or later)
+
+// #define TFT_CS 14  //for D32 Pro
+// #define TFT_DC 27  //for D32 Pro
+// #define TFT_RST 33 //for D32 Pro
+// #define TS_CS  12 //for D32 Pro
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
+XPT2046_Touchscreen ts(TS_CS);
+
+void setup()
+{
+  Serial.begin(115200);
+  ts.begin();
+  ts.setRotation(1);
+
+  tft.begin();
+  tft.setRotation(3);
+  tft.fillScreen(ILI9341_BLACK);
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(3);
+  tft.println("Touch Test");
+
+  while (!Serial && (millis() <= 1000))
+    ;
+}
+
+void loop()
+{
+  if (ts.touched())
+  {
+    TS_Point p = ts.getPoint();
+
+    tft.fillScreen(ILI9341_BLACK);
+    tft.setCursor(0, 0);
+
+    tft.print("Pressure = ");
+    tft.println(p.z);
+    tft.print("X = ");
+    tft.println(p.x);
+    tft.print("Y = ");
+    tft.println(p.y);
+  }
+}
+
---
+++ examples/TFT2p4_touchpaint/TFT2p4_touchpaint.ino
@@ -0,0 +1,119 @@
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#include <XPT2046_Touchscreen.h>
+
+#define TFT_CS D0  //for D1 mini or TFT I2C Connector Shield (V1.1.0 or later)
+#define TFT_DC D8  //for D1 mini or TFT I2C Connector Shield (V1.1.0 or later)
+#define TFT_RST -1 //for D1 mini or TFT I2C Connector Shield (V1.1.0 or later)
+#define TS_CS D3   //for D1 mini or TFT I2C Connector Shield (V1.1.0 or later)
+
+// #define TFT_CS 14  //for D32 Pro
+// #define TFT_DC 27  //for D32 Pro
+// #define TFT_RST 33 //for D32 Pro
+// #define TS_CS  12 //for D32 Pro
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
+XPT2046_Touchscreen ts(TS_CS);
+
+
+
+#include <Adafruit_GFX.h>    // Core graphics library
+
+// This is calibration data for the raw touch data to the screen coordinates
+#define TS_MINX 3800
+#define TS_MAXX 100
+#define TS_MINY 100
+#define TS_MAXY 3750
+
+// Size of the color selection boxes and the paintbrush size
+#define BOXSIZE 40
+#define PENRADIUS 3
+int oldcolor, currentcolor;
+
+void setup(void) {
+  Serial.begin(115200);
+
+  delay(10);
+  Serial.println("FeatherWing TFT");
+  if (!ts.begin()) {
+    Serial.println("Couldn't start touchscreen controller");
+    while (1);
+  }
+  ts.setRotation(4);
+
+  Serial.println("Touchscreen started");
+  
+  tft.begin();
+  tft.setRotation(2);
+  tft.fillScreen(ILI9341_BLACK);
+  
+  // make the color selection boxes
+  tft.fillRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_RED);
+  tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_YELLOW);
+  tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, ILI9341_GREEN);
+  tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, ILI9341_CYAN);
+  tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, ILI9341_BLUE);
+  tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, ILI9341_MAGENTA);
+ 
+  // select the current color 'red'
+  tft.drawRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
+  currentcolor = ILI9341_RED;
+}
+
+void loop() {
+  // Retrieve a point  
+  TS_Point p = ts.getPoint();
+  
+  Serial.print("X = "); Serial.print(p.x);
+  Serial.print("\tY = "); Serial.print(p.y);
+  Serial.print("\tPressure = "); Serial.println(p.z);  
+ 
+ 
+  // Scale from ~0->4000 to tft.width using the calibration #'s
+  p.x = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
+  p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
+
+  if (p.y < BOXSIZE) {
+     oldcolor = currentcolor;
+
+     if (p.x < BOXSIZE) { 
+       currentcolor = ILI9341_RED; 
+       tft.drawRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
+     } else if (p.x < BOXSIZE*2) {
+       currentcolor = ILI9341_YELLOW;
+       tft.drawRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
+     } else if (p.x < BOXSIZE*3) {
+       currentcolor = ILI9341_GREEN;
+       tft.drawRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
+     } else if (p.x < BOXSIZE*4) {
+       currentcolor = ILI9341_CYAN;
+       tft.drawRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
+     } else if (p.x < BOXSIZE*5) {
+       currentcolor = ILI9341_BLUE;
+       tft.drawRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
+     } else if (p.x < BOXSIZE*6) {
+       currentcolor = ILI9341_MAGENTA;
+       tft.drawRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, ILI9341_WHITE);
+     }
+
+     if (oldcolor != currentcolor) {
+        if (oldcolor == ILI9341_RED) 
+          tft.fillRect(0, 0, BOXSIZE, BOXSIZE, ILI9341_RED);
+        if (oldcolor == ILI9341_YELLOW) 
+          tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, ILI9341_YELLOW);
+        if (oldcolor == ILI9341_GREEN) 
+          tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, ILI9341_GREEN);
+        if (oldcolor == ILI9341_CYAN) 
+          tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, ILI9341_CYAN);
+        if (oldcolor == ILI9341_BLUE) 
+          tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, ILI9341_BLUE);
+        if (oldcolor == ILI9341_MAGENTA) 
+          tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, ILI9341_MAGENTA);
+     }
+  }
+
+  if (((p.y-PENRADIUS) > 0) && ((p.y+PENRADIUS) < tft.height())) {
+    tft.fillCircle(p.x, p.y, PENRADIUS, currentcolor);
+  }
+}
